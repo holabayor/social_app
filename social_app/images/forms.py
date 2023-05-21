e09f183d2e7168a28a45a71fce6d3bba7a04bca7
holabayor <liasu.olabayo@gmail.com>
2023-05-21 21:36:40 +0100

    follow and unfollow done

--- social_app/images/forms.py
+++ social_app/images/forms.py
@@ -8,26 +8,27 @@ from .models import Image
 class ImageCreateForm(forms.ModelForm):
     class Meta:
         model = Image
-        fields = ['title', 'url', 'description']
+        fields = ["title", "url", "description"]
         widgets = {
-            'url': forms.HiddenInput,
+            "url": forms.HiddenInput,
         }
 
     def clean_url(self):
-        url = self.cleaned_data['url']
-        valid_extensions = ['jpg', 'jpeg', 'png']
-        extension = url.rsplit('.', 1)[1].lower()
+        url = self.cleaned_data["url"]
+        valid_extensions = ["jpg", "jpeg", "png"]
+        extension = url.rsplit(".", 1)[1].lower()
         if extension not in valid_extensions:
             raise forms.ValidationError(
-                'The given URL does not match valid image extensions.')
+                "The given URL does not match valid image extensions."
+            )
         return url
 
     def save(self, force_insert=False, force_update=False, commit=True):
         image = super().save(commit=False)
-        image_url = self.cleaned_data['url']
+        image_url = self.cleaned_data["url"]
         name = slugify(image.title)
-        extension = image_url.rsplit('.', 1)[1].lower()
-        image_name = f'{name}.{extension}'
+        extension = image_url.rsplit(".", 1)[1].lower()
+        image_name = f"{name}.{extension}"
         # download the image from the url
         response = requests.get(image_url)
         image.image.save(image_name, ContentFile(response.content), save=False)
