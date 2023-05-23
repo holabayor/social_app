3773f299f1d16aead2087a98f5927a865163b4ba
holabayor <liasu.olabayo@gmail.com>
2023-05-23 11:21:25 +0100

    activity feeds

--- social_app/images/apps.py
+++ social_app/images/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class ImagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'images'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "images"
+
+    def ready(self) -> None:
+        # import signals handlers
+        import images.signals
